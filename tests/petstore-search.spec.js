c7c8d8881a33bc690050639dac88e8e62f8b55b7
joanaraposo96 <joanaraposo96@outlook.pt>
2026-04-11 16:23:07 +0200

    Added comments to explain variable scope

--- tests/petstore-search.spec.js
+++ tests/petstore-search.spec.js
@@ -3,13 +3,13 @@ import Header from '../pom/Header.js';
 import Homepage from '../pom/Homepage.js';
 
 test.describe('Petstore Search', () => {
-    let header;
+    let header; // variable declared outside of beforeEach, so it can be used in all tests within this describe block.
     let homepage;
     let category;
     let searchResult;
     
     test.beforeEach(async ({ page }) => {
-        header = new Header(page);
+        header = new Header(page); // vs. let header -> local variable, this means it would not be accessible in other tests, so we need to declare it outside of beforeEach.
         homepage = new Homepage(page);
         await homepage.navigateToHomepage();
     });
